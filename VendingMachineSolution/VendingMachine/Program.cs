44ce35a781d469d43feee86f814a27eeebae7ee1
Catalin Schiau <ratty_4_ana@yahoo.com>
2020-02-03 20:59:26 +0200

    defined products and money message

--- VendingMachineSolution/VendingMachine/Program.cs
+++ VendingMachineSolution/VendingMachine/Program.cs
@@ -11,6 +11,17 @@ namespace VendingMachine
         static void Main(string[] args)
         {
             string[] pepsi = { "pepsi", "5","10","11"};
+            string[] cola = { "coca-cola", "5", "9", "12"};
+            string[] sicola = { "sicola", "2.5", "5", "13" };
+            string[][] vendingMachineStationQ7 = { pepsi, cola, sicola };
+            foreach (string[] product in vendingMachineStationQ7)
+            {
+                Console.WriteLine(product[3] + " " + product[0] + " " + product[1] + " LEI");
+            }
+
+            Console.WriteLine("Please enter money");
+            decimal userAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Entered amount " + userAmount);
         }
     }
 }
