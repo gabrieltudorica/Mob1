7dcb307425e65ddacff640927ad7503534051367
anamaria6 <anamariatotan6@gmail.com>
2020-02-13 21:10:22 +0200

    added SMS functionality

--- VendingMachineSolution/VendingMachine/Program.cs
+++ VendingMachineSolution/VendingMachine/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -134,6 +135,20 @@ namespace VendingMachine
             int updatedStock = Convert.ToInt32(selectedProduct[2]) - 1;
             selectedProduct[2] = updatedStock.ToString();
             Console.WriteLine("The transaction is successful. Money left: " + userAmount);
+
+            if (updatedStock == 0)
+            {
+                SendSMS("You are out of " + selectedProduct[0]);
+            }
+        }
+
+        private static void SendSMS(string message)
+        {
+            string parameter = string.Format("/C \"C:\\Users\\anamaria.totan\\Downloads\\curl-7.68.0-win64-mingw\\curl-7.68.0-win64-mingw\\bin\\curl --request POST --header \"X-Authorization: {0}\" \"https://app.smso.ro/api/v1/send\" -d \"sender=4\" -d \"to={1}\" -d \"body={2}\"\"",
+                "api-key",
+                "+40number",
+                message);
+            Process.Start("cmd.exe", parameter);
         }
 
     }
