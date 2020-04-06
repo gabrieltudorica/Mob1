9ffe4b8c85828d3a523aaf5a9722fd43c61c083d
Gabriel Tudorica <gabriel.tudorica@gmail.com>
2020-04-07 00:59:45 +0300

    Mocks

---
+++ ObjectOrientedProgramming/Mocks/ClassLibrary1/VendingMachine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class SmsNotAvailableException : Exception { }
+
+    public class VendingMachine
+    {
+        private ISmsGateway smsGateway;
+        private int stock = 2;
+        
+        public VendingMachine(ISmsGateway smsGateway)
+        {
+            this.smsGateway = smsGateway;
+        }
+
+        public void Purchase()
+        {
+            stock--;
+
+            if (stock == 0)
+            {
+                bool boss1Result = smsGateway.SendSms("0712345678", "Am ramas fara marfa, boss1!");
+                
+                if (!boss1Result)
+                { 
+                    bool boss2Result = smsGateway.SendSms("0722345678", "Am ramas fara marfa, boss2!");
+                    
+                    if (!boss2Result)
+                    {
+                        throw new SmsNotAvailableException();
+                    }
+                }                
+            }
+        }
+    }
+}
---
+++ ObjectOrientedProgramming/Mocks/UnitTestProject1/VendingMachineTests.cs
@@ -0,0 +1,60 @@
+﻿
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class VendingMachineTests
+    {
+        [TestMethod]
+        public void WhenStockIsZeroAndFirstOwnerIsAvailable_Purchase_SendsSmsToOwner()
+        {
+            //arrange
+            var mock = new Mock<ISmsGateway>();
+            mock.Setup(x => x.SendSms("0712345678", It.IsAny<string>())).Returns(true);
+
+            var vm = new VendingMachine(mock.Object);
+            vm.Purchase();
+            
+            //act
+            vm.Purchase();
+
+            //assert
+            mock.Verify(x => x.SendSms("0712345678", It.IsAny<string>()), Times.Exactly(1));
+        }
+
+        [TestMethod]
+        public void WhenStockIsZeroAndFirstOwnerUnavailable_Purchase_SendsSmsToSecondOwner()
+        {
+            //arrange
+            var mock = new Mock<ISmsGateway>();
+            mock.Setup(x => x.SendSms("0712345678", It.IsAny<string>())).Returns(false);
+            mock.Setup(x => x.SendSms("0722345678", It.IsAny<string>())).Returns(true);
+            var vm = new VendingMachine(mock.Object);
+            vm.Purchase();
+
+            //act
+            vm.Purchase();
+
+            //assert
+            mock.Verify(x => x.SendSms("0722345678", It.IsAny<string>()), Times.Exactly(1));
+        }
+
+        [ExpectedException(typeof(SmsNotAvailableException))]
+        [TestMethod]
+        public void WhenStockIsZeroAndBothOwnersUnavailable_Purchase_ThrowsException()
+        {
+            //arrange
+            var mock = new Mock<ISmsGateway>();
+            mock.Setup(x => x.SendSms("0712345678", It.IsAny<string>())).Returns(false);
+            mock.Setup(x => x.SendSms("0722345678", It.IsAny<string>())).Returns(false);            
+            var vm = new VendingMachine(mock.Object);
+            vm.Purchase();
+
+            //act
+            vm.Purchase();                      
+        }
+    }
+}
